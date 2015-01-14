0168a888a8cc82a4c4cbf53b48bb61c1861d1c8b
REV3NT3CH <rev3nt3ch@gmail.com>
2015-01-13 19:45:21 -0800

    Liquify

---
+++ liquid.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d803/d803.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d803
+PRODUCT_NAME := liquid_d803
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D803
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_ca/g2:4.4.2/KOT49I.D803B20e/D803B20e.1394602420:user/release-keys
+PRIVATE_BUILD_DESC="g2_ca-user 4.4.2 KOT49I D803B20e.1394602420 release-keys"
+
+PRODUCT_PACKAGES += Torch
